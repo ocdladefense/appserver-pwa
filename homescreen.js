b83386787a152f102c49d01370c9434835c8fdd7
jack <brainard@my.lanecc.edu>
2020-01-23 15:11:38 -0800

    homescreen js file added

---
+++ homescreen.js
@@ -0,0 +1,20 @@
+window.onload=function(){
+deferredPrompt=window.deferredPrompt;
+var btnAdd = document.getElementById('btnAdd');
+btnAdd.addEventListener('click', (e) => {
+  // hide our user interface that shows our A2HS button
+  btnAdd.style.display = 'none';
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice
+    .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+});
+}
