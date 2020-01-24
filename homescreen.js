40132f74ede0382d029ced10b9e9569b9a734625
jack <brainard@my.lanecc.edu>
2020-01-24 15:03:26 -0800

    add to home screen button now working

--- homescreen.js
+++ homescreen.js
@@ -1,4 +1,12 @@
-window.onload=function(){
+deferredPrompt = window.deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  showInstallPromotion(e);
+});
+
+showInstallPromotion=function(e){
 deferredPrompt=window.deferredPrompt;
 var btnAdd = document.getElementById('btnAdd');
 btnAdd.addEventListener('click', (e) => {
@@ -10,9 +18,9 @@ btnAdd.addEventListener('click', (e) => {
   deferredPrompt.userChoice
     .then((choiceResult) => {
       if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
+        
       } else {
-        console.log('User dismissed the A2HS prompt');
+        
       }
       deferredPrompt = null;
     });
