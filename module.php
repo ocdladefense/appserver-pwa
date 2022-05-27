9277f85559b33055968831120d433a6a8db311d1
Jose Bernal <jbernal.web.dev@gmail.com>
2022-05-26 21:51:28 -0700

    Add route for retrieving manifest from uploades.

--- module.php
+++ module.php
@@ -2,11 +2,19 @@
 
 
 
-class PwaModule extends Module {
+class PwaModule extends Module
+{
+	public function __construct()
+	{
+		parent::__construct();
 
-
-	public function __construct(){
-
+		$this->name = "pwa";
 	}
 
+	public function manifest()
+	{
+		
+		$manifest = file_get_contents(BASE_PATH . "/content/uploads/modules/pwa/manifest.json");
+		return $manifest;
+	}
 }
