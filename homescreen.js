fe26f5b5da88e0c3b0d5c2584bf6f0c0815b6035
Jose Bernal <jbernal.web.dev@gmail.com>
2022-05-26 21:52:48 -0700

    Unknown.

--- homescreen.js
+++ homescreen.js
@@ -6,23 +6,32 @@ window.addEventListener('beforeinstallprompt', (e) => {
   showInstallPromotion(e);
 });
 
-showInstallPromotion=function(e){
-deferredPrompt=window.deferredPrompt;
-var btnAdd = document.getElementById('btnAdd');
-btnAdd.addEventListener('click', (e) => {
-  // hide our user interface that shows our A2HS button
-  btnAdd.style.display = 'none';
-  // Show the prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice
-    .then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        
-      } else {
-        
-      }
-      deferredPrompt = null;
-    });
-});
-}
+showInstallPromotion = function(e) {
+
+
+
+	deferredPrompt = window.deferredPrompt;
+
+	var btnAdd = document.getElementById('btnAdd');
+	
+	btnAdd.addEventListener('click', (e) => {
+	
+		// hide our user interface that shows our A2HS button
+		btnAdd.style.display = 'none';
+		
+		// Show the prompt
+		deferredPrompt.prompt();
+		
+		// Wait for the user to respond to the prompt
+		deferredPrompt.userChoice
+		.then((choiceResult) => {
+			if (choiceResult.outcome === 'accepted') {
+			
+			} else {
+			
+			}
+			deferredPrompt = null;
+		});
+		
+	});
+};
